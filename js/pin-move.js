e2d822e778109d8f004d16b8164196eb6142d9af
Елена <elenalecs@rambler.ru>
2020-03-08 22:28:46 +0300

    Добавление перемещения метки/добавление валидации формы

---
+++ js/pin-move.js
@@ -0,0 +1,40 @@
+'use strict';
+(function () {
+
+
+  window.pageStatus.mainPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoordinate = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoordinate.x - moveEvt.clientX,
+        y: startCoordinate.y - moveEvt.clientY
+      };
+
+      startCoordinate = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      window.pageStatus.mainPin.style.top = (window.pageStatus.mainPin.offsetTop - shift.y) + 'px';
+      window.pageStatus.mainPin.style.left = (window.pageStatus.mainPin.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
+})();
